ad28d71d0ed5906eef715f70402bfc76181c23a4
Leonid Shishkin <lshishkin150586@gmail.com>
2026-01-14 22:14:16 +0500

    feature: remove docker

--- src/migrations/1768408375704-Create.ts
+++ src/migrations/1768408375704-Create.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Create1767873584322 implements MigrationInterface {
+export class Create1768408375704 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
