517d99cef40137a680829bd2c0b4c83903c46915
Leonid Shishkin <lshishkin150586@gmail.com>
2026-01-24 04:05:38 +0500

    feat: изменен путь

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
